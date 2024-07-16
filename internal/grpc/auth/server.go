f533b876c57f1fae8b5ed4eab098f7166dd312c3
mazxcv <mazxccc@gmail.com>
2024-07-17 02:02:15 +0300

    feature-login add login handler m.ugirn

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -5,25 +5,60 @@ import (
 
 	ssov1 "github.com/mazxcv/protos/gen/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appId int,
+	) (token string, err error)
+
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+
+	IsAdmin(ctx context.Context, userID int64) (bool, error)
+}
 
 // Надо искать в протофайлах
 // дает заглушку
 // Иначе потребовалось бы реализовывать весь интерфейс Login, Register, IsAdmin
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
+
+const (
+	emptyValue = 0
+)
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
+
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+	// TODO implement auth service
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO ...
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
 	return &ssov1.LoginResponse{
-		Token: req.GetEmail(),
+		Token: token,
 	}, nil
 }
 
@@ -39,4 +74,20 @@ func (s *serverAPI) IsAdmin(
 	req *ssov1.IsAdminRequest,
 ) (*ssov1.IsAdminResponse, error) {
 	panic("implement me")
+}
+
+func validateLogin(req *ssov1.LoginRequest) error {
+	if req.GetEmail() == "" {
+		return status.Error(codes.InvalidArgument, "email not is empty")
+	}
+
+	if req.GetPassword() == "" {
+		return status.Error(codes.InvalidArgument, "password is required")
+	}
+
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
 }
